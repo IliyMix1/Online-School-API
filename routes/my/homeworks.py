a8441d4cf986b81e63b72ff2cbeec873e991a8a4
Илья Мышьяков <ilyamyshy228@gmail.com>
2026-05-18 14:29:47 +0500

    Rewrote the route for submitting homework, now it actually let's user to pass homework and script will count how many points he earned. Also added some comments to schemas.py and removed cache

--- routes/my/homeworks.py
+++ routes/my/homeworks.py
@@ -1,6 +1,6 @@
 #Для эндпоинтов
 from fastapi                import APIRouter, Depends, HTTPException
-from schemas.schemas        import SubmissionHomework, TaskForStudent
+from schemas.schemas        import SubmissionHomework, TaskForStudent, HomeworkSubmit, TaskSubmit
 #Для интеграции с PostgreSQL
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy             import select
@@ -46,7 +46,7 @@ async def get_homework(homework_id: int, session: AsyncSession = Depends(get_ses
     return tasks
 
 @my_router.post('/homeworks/{homework_id}/submit')
-async def submit_homework(homework_id: int, score: int, session: AsyncSession = Depends(get_session), user = Depends(get_current_user)):
+async def submit_homework(homework_id: int, submitted_tasks: list[TaskSubmit], session: AsyncSession = Depends(get_session), user = Depends(get_current_user)):
     #Проверяем есть ли такая домашка в БД
     result = await session.execute(
         select(Homework).where(Homework.homework_id == homework_id)
@@ -58,11 +58,71 @@ async def submit_homework(homework_id: int, score: int, session: AsyncSession = 
     
     #Проверяем достаточно ли у пользователя прав, чтобы посмотреть эту домашку
     enrollment = await get_enrollment(course_id=homework.course_id, user=user, session=session)
-    
-    submission_data = SubmissionHomework(
+
+    #Достаём из таблицы все задачки, которые привязаны к домашке
+    result = await session.execute(
+        select(Task).join(HomeworkTask, HomeworkTask.task_id == Task.task_id).where(HomeworkTask.homework_id == homework_id)  #Склеили 2 таблицы по нашему "правилу", а затем отфильтровали результат
+    )
+    tasks = result.scalars().all()
+
+    #Создаём список с правильными ответами вида: {'task_id': 'correct_answer'}
+    correct_answers = {}
+    for task in tasks:
+        correct_answers[task.task_id] = {
+            task.task_id: task.correct_answer
+        }
+
+    #Создаём список с введёнными ответами вида: {'task_id': 'answer'}
+    submitted_answers = {}
+    for task in submitted_tasks:
+        submitted_answers[task.task_id] = {
+            task.task_id: task.answer
+        }
+
+    earned_points = 0
+    max_points = 0
+    #Проходимся по правильным ответам и сравниваем их с тем, что отправил пользователь
+    for task_id, correct_answer in correct_answers.items():
+        #Достаём из словаря ответ пользователя по id
+        submitted_answer = submitted_answers.get(task_id)
+
+        max_points += 1
+        if submitted_answer == correct_answer:
+            earned_points += 1
+            print('Wow, cool!')
+        else:
+            print('Try again later, bro')
+
+    data = SubmissionHomework(
         enrollment_id=enrollment.enrollment_id,
         homework_id=homework_id,
-        score=score,
+        score=earned_points,
     )
+    return await create_record(model=Submission, schema=data, session=session)
 
-    return await create_record(model=Submission, schema=submission_data, session=session)
+    #return {'earned_points': earned_points, 'max_points': max_points}
+
+
+
+
+# @my_router.post('/homeworks/{homework_id}/submit')
+# async def submit_homework(homework_id: int, score: int, session: AsyncSession = Depends(get_session), user = Depends(get_current_user)):
+#     #Проверяем есть ли такая домашка в БД
+#     result = await session.execute(
+#         select(Homework).where(Homework.homework_id == homework_id)
+#     )
+#     homework = result.scalar_one_or_none()
+
+#     if homework is None:
+#         raise HTTPException(status_code=404, detail='Not found')
+    
+#     #Проверяем достаточно ли у пользователя прав, чтобы посмотреть эту домашку
+#     enrollment = await get_enrollment(course_id=homework.course_id, user=user, session=session)
+    
+#     submission_data = SubmissionHomework(
+#         enrollment_id=enrollment.enrollment_id,
+#         homework_id=homework_id,
+#         score=score,
+#     )
+
+#     return await create_record(model=Submission, schema=submission_data, session=session)
--- schemas/schemas.py
+++ schemas/schemas.py
@@ -60,20 +60,24 @@ from datetime import date
 
 
 class AuthReg(BaseModel):
+    '''Для регистрации'''
     first_name: str = Field(max_length=50)
     last_name:  str = Field(max_length=50)
-    email:    str = Field(max_length=320)
-    password: str = Field(min_length=8, max_length=70)
+    email:      str = Field(max_length=320)
+    password:   str = Field(min_length=8, max_length=70)
 
 class AuthLogin(BaseModel):
+    '''Для логина'''
     email:    str = Field(max_length=320)
     password: str = Field(min_length=8, max_length=70)
 
 class AuthUserCreate(BaseModel):
+    '''Для создания записи в таблице с хэшами при регистрации'''
     hashed_password: str 
     role: Literal['student', 'admin']
 
 class AuthStudentCreate(BaseModel):
+    '''Для создания записи в таблице с контактной информацией при регистрации'''
     user_id:    int
     email:      str = Field(max_length=320)
     first_name: str = Field(max_length=50)
@@ -81,21 +85,25 @@ class AuthStudentCreate(BaseModel):
 
 
 class UserCreate(BaseModel):
+    '''Для создания записи в таблице с хэшами вручную'''
     hashed_password: str = Field(min_length=2)
     role: Literal['student', 'admin']
 
 class UserPatch(BaseModel):
+    '''Для изменения пароля или роли'''
     hashed_password: str | None = None
     role: Literal['student', 'admin'] | None = None
 
 
 class StudentOut(BaseModel):
+    '''Для отображения контактной информации ученика'''
     first_name:   str
     last_name:    str
     email:        str
     phone_number: str | None = None
 
 class StudentPatch(BaseModel):
+    '''Для точечного изменения контактной информации ученика'''
     first_name:   str | None = None
     last_name:    str | None = None
     email:        str | None = None
@@ -103,50 +111,69 @@ class StudentPatch(BaseModel):
 
 
 class CourseCreate(BaseModel):
+    '''Для создания курса'''
     name: str = Field(min_length=2)
 
 class CoursePatch(BaseModel):
+    '''Для точечного изменения курса'''
     name: str | None = None
 
 class CourseOut(CourseCreate):
+    '''Для отображения курса'''
     course_id:  int
     created_at: date
 
 
 class EnrollmentBuy(BaseModel):
+    '''Для покупки курса'''
     tariff:     Literal['mini', 'standard', 'pro']
 
 class EnrollmentCreate(BaseModel):
+    '''Для создания записи после покупки курса'''
     user_id:    int
     course_id:  int
     tariff:     Literal['mini', 'standard', 'pro']
     status:     Literal['active', 'finished', 'quit']
 
 class EnrollmentPatch(BaseModel):
-    #user_id:   int  | None = None
+    '''Для точечного изменения данных в купленных курсах ученика'''
     course_id: int  | None = None
     tariff:    Literal['mini', 'standard', 'pro']    | None = None
     status:    Literal['active', 'finished', 'quit'] | None = None
     ended_at:  date | None = None
 
 class EnrollmentOut(EnrollmentCreate):
+    '''Для отображения купленного курса'''
     enrollment_id: int
     created_at:    date
     ended_at:      date | None
 
 
 class SubmissionHomework(BaseModel):
+    '''Для записи в таблицу всех сданных работ'''
     enrollment_id: int
     homework_id:   int
     score:         int
 
 
 class AttendanceLesson(BaseModel):
+    '''Для записи в таблицу с посещаемостью'''
     enrollment_id: int
     lesson_id:     int
 
 
 class TaskForStudent(BaseModel):
+    '''Для отображения заданий'''
+    task_id:   int
+    task_text: str
+    points:    int
+
+
+class TaskSubmit(BaseModel):
+    '''Для сдачи заданий'''
     task_id: int
-    task_text:   str
-    points:      int
+    answer: str = Field(min_length=1)
+
+class HomeworkSubmit(BaseModel):
+    '''Для сдачи домашек'''
+    list[TaskSubmit]
