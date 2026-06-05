7e96f484d12c4f160f83ec3854c77197a538813e
Илья Мышьяков <ilyamyshy228@gmail.com>
2026-06-05 13:43:25 +0500

    Added a README.md file and commented one file to work with it later on

---
+++ routes/admin/tasks.py
@@ -0,0 +1,13 @@
+# #Для эндпоинтов
+# from fastapi                import APIRouter, Depends, HTTPException
+# from schemas.schemas        import HomeworkCreate
+# #Для интеграции с PostgreSQL
+# from sqlalchemy.ext.asyncio import AsyncSession
+# from sqlalchemy             import select
+# from database               import get_session 
+# from models.models          import Homework, HomeworkTask, Course, Lesson, Task
+# #Зависимости
+# from dependencies           import get_current_admin
+
+# admin_router = APIRouter(prefix='/admin', tags=['Tasks'])
+
