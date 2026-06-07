f6163c13faa673a220e31be0abebc36955beee02
Илья Мышьяков <ilyamyshy228@gmail.com>
2026-06-07 19:28:56 +0500

    Added test for registration. Also added a template for login-test, but test must be async, so there is a problem(422)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,45 @@
 from fastapi.testclient import TestClient
 from main import app
 
-from database import get_session, get_session_test
+from database import get_session, get_session_test, create_record
+
+import random
 
 #Подменяем сессию на тестовую, чтобы все запросы отправлялись в тестовую БД
 app.dependency_overrides[get_session] = get_session_test
 
 client = TestClient(app)
 
+
+def generate_filler(num: int) -> str:
+    '''Создаёт случайный наполнитель для различных полей'''
+    symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+    filler = ''
+    for i in range(num):
+        filler += random.choice(symbols)
+
+    return filler
+
+
 def test_simple():
     assert 1 + 1 == 2
 
 def test_reg():
-    response = client.post('/auth/reg', json={"first_name": 'Jane', "last_name": 'Doe', "email": 'test123@gmail.com', "password": 'test12345'})
+    '''Проверяем эндпоинт регистрации'''
+    response = client.post('/auth/reg', json={
+        "first_name": f'name_{generate_filler(15)}', 
+        "last_name":  f'surname_{generate_filler(15)}', 
+        "email":      f'user_{generate_filler(20)}@gmail.com', 
+        "password":    'test12345'
+        })
     assert response.status_code == 200
 
-# def test_get_all_students():
-#     response = client.get('/students')
-#     assert response.status_code == 200
+def test_login():
+    '''Проверяем эндпоинт логина'''
+    response = client.post('/auth/login', json={
+        "email": 'adminROOT@gmail.com',
+        "password": 'Admin12345'
+    })
+    print(response.json())
+    assert response.status_code == 200
